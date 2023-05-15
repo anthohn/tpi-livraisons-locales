6469904e9919024b9ce2102a4d92e7446a3de574
Anthony Höhn <anthonyhohn@gmail.com>
2023-05-15 10:05:52 +0200

    détails product + small design changes

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -12,27 +12,47 @@ class ProductController extends AbstractController
     #[Route('/', name: 'home')]
     public function Redirection()
     {
+        //redirect to home page
         return $this->redirectToRoute('app_home');
     }
 
     #[Route('/accueil', name: 'app_home')]
     public function index(): Response
     {
+        // home page
         return $this->render('product/index.html.twig', [
             'controller_name' => 'ProductController',
         ]);
     }
 
-    #[Route('/produits', name: 'app_product')]
-    public function product(TProductRepository $TProductRepository): Response
+    #[Route('/produits', name: 'app_products')]
+    public function products(TProductRepository $TProductRepository): Response
     {
-        // get all products
+        // get all products in db
         $products = $TProductRepository->findAll();
 
 
-        return $this->render('product/product.html.twig', [
+        return $this->render('product/products.html.twig', [
             'controller_name' => 'ProductController',
             'products' => $products
         ]);
     }
+
+    #[Route('/produit/{id}', name: 'product_show')]
+    public function show(int $id, TProductRepository $TProductRepository): Response
+    {
+        //retrieve the product via given id
+        $product = $TProductRepository->find($id);
+        
+        //Check if $product is valid and redirect to home if null
+        if (!$product) {
+            return $this->redirectToRoute('home');
+        }
+      
+        return $this->render('product/product.html.twig', [
+            'controller_name' => 'ProductController',
+            'product' => $product
+        ]);
+    }
+    
 }
